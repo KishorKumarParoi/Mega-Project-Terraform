7387a66cf947d2849d5542f15aaf838b20590962
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2026-03-08 21:06:57 +0600

    Implement EBS CSI driver with IAM role and policies
    Added IAM role and policies for EBS CSI driver, updated EKS addon configuration, and modified remote access settings.

--- main.tf
+++ main.tf
@@ -96,24 +96,74 @@ resource "aws_eks_cluster" "kkp" {
   }
 }
 
+# Get AWS account ID
+data "aws_caller_identity" "current" {}
+
+# Get latest EBS CSI addon version
+data "aws_eks_addon_version" "ebs" {
+  addon_name         = "aws-ebs-csi-driver"
+  kubernetes_version = aws_eks_cluster.kkp.version
+  most_recent        = true
+}
+
+# Create IAM role for EBS CSI driver addon
+resource "aws_iam_role" "ebs_csi_driver" {
+  name_prefix = "ebs-csi-driver-"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${replace(aws_eks_cluster.kkp.identity[0].oidc[0].issuer, "https://", "")}"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(aws_eks_cluster.kkp.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+            "${replace(aws_eks_cluster.kkp.identity[0].oidc[0].issuer, "https://", "")}:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# Attach policy to EBS CSI driver role
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.ebs_csi_driver.name
+}
+
 
 resource "aws_eks_addon" "ebs_csi_driver" {
-  cluster_name    = aws_eks_cluster.kkp.name
-  addon_name      = "aws-ebs-csi-driver"
-  
+  cluster_name             = aws_eks_cluster.kkp.name
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = data.aws_eks_addon_version.ebs.version 
+  service_account_role_arn = aws_iam_role.ebs_csi_driver.arn        
+
   timeouts {
-    create = "30m"
-    update = "30m"
-    delete = "15m"
+    create = "10m"   
+    update = "10m"
+    delete = "5m"
   }
-  
+
   resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
-  
-  # Prevent destruction on terraform apply/destroy
+  resolve_conflicts_on_update = "PRESERVE" 
+
   lifecycle {
     prevent_destroy = true
-    ignore_changes  = [tags]
+  }
+
+  depends_on = [
+    aws_eks_cluster.kkp,
+    aws_eks_node_group.kkp,
+    aws_iam_role_policy_attachment.ebs_csi_driver
+  ]
+
+  tags = {
+    Name = "ebs-csi-driver"
   }
 }
 
@@ -122,6 +172,7 @@ resource "aws_eks_node_group" "kkp" {
   node_group_name = "kkp-node-group"
   node_role_arn   = aws_iam_role.kkp_node_group_role.arn
   subnet_ids      = aws_subnet.kkp_subnet[*].id
+  version         = aws_eks_cluster.kkp.version
 
   scaling_config {
     desired_size = 3
@@ -131,12 +182,24 @@ resource "aws_eks_node_group" "kkp" {
 
   instance_types = ["t2.large"]
 
-  remote_access {
-    ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.kkp_node_sg.id]
+  dynamic "remote_access" {
+    for_each = var.ssh_key_name != "" ? [1] : []
+    content {
+      ec2_ssh_key               = var.ssh_key_name
+      source_security_group_ids = [aws_security_group.kkp_node_sg.id]
+    }
   }
-}
 
+  tags = {
+    Name = "kkp-node-group"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.kkp_node_group_role_policy,
+    aws_iam_role_policy_attachment.kkp_node_group_cni_policy,
+    aws_iam_role_policy_attachment.kkp_node_group_registry_policy,
+  ]
+}
 resource "aws_iam_role" "kkp_cluster_role" {
   name = "kkp-cluster-role"
 
